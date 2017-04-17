95c7d15e6222f59ce5f3dc59c01d4787a501b5ac
rmit-s3379835-angus-scheid <s3379835@student.rmit.edu.au>
2017-04-17 19:50:17 +1000

    Added an enum to the cell class.

--- src/strategicGame/cell.java
+++ src/strategicGame/cell.java
@@ -1,5 +1,18 @@
 package strategicGame;
 
-public class cell {
+public class cell 
+{
+
+	public enum Contents
+	{
+		EMPTY
+		MONSTER
+		PLAYER
+		FOOD
+		CONTACTPLAYERMONSTER
+		CONTACTPLAYERFOOD
+		CONTACTMONSTERFOOD
+		IMPASSABLE
+	}
 
 }
