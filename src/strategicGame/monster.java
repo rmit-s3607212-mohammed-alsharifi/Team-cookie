d0681276b3e4f8fde38f8bdfb8f7b0ed35ee013f
vinhduy-nguyen <s3588237@student.rmit.edu.au>
2017-04-18 12:40:49 +1000

    monster simple

--- src/strategicGame/monster.java
+++ src/strategicGame/monster.java
@@ -1,5 +1,24 @@
 package strategicGame;
 
-public class monster {
+public class monster 
+{
+	private int currentspeed = 2;
+	private int x;
+	private int y;
+	public monster(int x, int y)
+	{
+		if((x>Map.width) || (y>Map.height) || ((x=hollowArea.x) && (y=hollAwarea.y)))
+		{
+			System.out.println("not available");
+		}
+		else
+		{
+			monster.x=x;
+			monster.y=y;
+		}
+	
+	}
+	
+	
 
 }
