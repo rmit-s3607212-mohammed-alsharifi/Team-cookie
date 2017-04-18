4d4a48a0b2202e9a982dd5cd1e0eecffaeabf9dd
vinhduy-nguyen <s3588237@student.rmit.edu.au>
2017-04-18 11:39:41 +1000

    simple login
    just login, not check

--- src/strategicGame/main.java
+++ src/strategicGame/main.java
@@ -1,11 +1,22 @@
 package strategicGame;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) 
 	{
-	
+	    String username;
+	    String password;
+	    Scanner input=new Scanner(System.in);
+	    
+	    
+		System.out.println("Welcome");
 		System.out.println("Please enter an username");
+		username=input.next();
+		System.out.println("Please enter a password");
+		password=input.next();
+	
 		
 		
 		
