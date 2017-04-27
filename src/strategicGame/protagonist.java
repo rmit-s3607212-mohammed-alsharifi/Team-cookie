0c329f4d14771e88f773b9f3acaec001f75271d6
rmit-s3379835-angus-scheid <s3379835@student.rmit.edu.au>
2017-04-27 12:49:44 +1000

    Revert "test 2"
    This reverts commit 3fdc9c74cd625c12fd4fa2d0913d752f17b0d3ce.

--- src/strategicGame/protagonist.java
+++ src/strategicGame/protagonist.java
@@ -1,39 +1,6 @@
 package strategicGame;
 
-public class protagonist 
+public class player 
 {
-	int cellX;
-	int cellY;
-	boolean isPoisoned;
-	boolean isDoublePoisoned;
-	boolean contactMonster;
-	boolean contactFood; 
-	int victoryTime;
-	int poisonTime;
-	int doublePoisonTime;	
-	
-	if(contactFood == true)
-	{
-		if(isPoisoned == true)
-		{
-			isDoublePoisoned = true;
-			doublePoisonTime = poisonTime; 
-			poisonTime = 20; 
-		}
-		else
-		{
-			isPoisoned = true;
-			poisonTime = 20;
-		}
-	}
-	
-	if(contactMonster == true)
-	{
-		System.out.println("Game over, you lose");
-	}
-	
-	if(victoryTime == 0)
-	{
-		System.out.println("Congratulations, you win")
-	}
+
 }
