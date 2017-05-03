bc0ec2a679a62ffadcdfbf474e3fe2f0ba89af91
s3600254 <s3600254@W8417028.rmit.internal>
2017-05-03 11:22:18 +1000

    food

---
+++ src/strategicGame/food.java
@@ -0,0 +1,15 @@
+package strategicGame;
+
+public class food extends cell {
+	
+	//upon spawn set time to 20 units
+	
+	//food placed in the position that player moves from
+	
+	//has coordinates that the player and monster are aware of
+	
+	//disappears after 20 units or upon collision with player
+	
+	//only 2 placed each game
+	
+}
